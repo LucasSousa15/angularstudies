c5ce86a486299470f5651bcc59217d7722a4a5d5
Lucas Sousa <111785664+LucasSousa15@users.noreply.github.com>
2024-12-13 14:34:24 -0300

    finish angular modules class

--- life-ciyle/src/app/app.module.ts
+++ life-ciyle/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { CheckSamplesComponent } from './check-samples/check-samples.component';
+import { ListaModule } from './lista/lista.module'
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CheckSamplesComponent } from './check-samples/check-samples.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ListaModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ life-ciyle/src/app/modulo-proj/modulo-proj.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modulo-proj',
+  standalone: true,
+  imports: [],
+  templateUrl: './modulo-proj.component.html',
+  styleUrl: './modulo-proj.component.css'
+})
+export class ModuloProjComponent {
+
+}
